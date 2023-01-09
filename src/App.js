bee44df48663cc0ce8a04b4f366f1a9b8cc5c07b
1Nelsonel <nelsonmokoro37@gmail.com>
2023-01-09 11:44:31 +0300

    success

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from './Components/About';
+import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Contact />
       <Footer />
     </div>
   );
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Contact = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="p-3" expand="sm" variant="dark">
+      <Container className="p-3">
+        <div className="p-3">
+          <h3>Contact Us</h3>
+        </div>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-export default Contact
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Telephone</Form.Label>
+                <Form.Control type="tel" placeholder="Telephone Number" />
+              </Form.Group>
+
+            </Col>
+            <Col>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
